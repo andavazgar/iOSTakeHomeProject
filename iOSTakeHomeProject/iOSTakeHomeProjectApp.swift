72584f160346afce6e7dc93055880b330e104aef
Andres Vazquez <andavazgar.dev@gmail.com>
2022-11-14 15:12:33 -0500

    Initial Commit

---
+++ iOSTakeHomeProject/iOSTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTakeHomeProjectApp.swift
+//  iOSTakeHomeProject
+//
+//  Created by Andres Vazquez on 2022-11-14.
+//
+
+import SwiftUI
+
+@main
+struct iOSTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
